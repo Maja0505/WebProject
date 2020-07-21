7855c5aa289e9fda6da88ba4310e2eaa08437584
Nemanja Jevtic <njevtic998@gmail.com>
2020-07-21 14:25:26 +0200

    #model_guest_and_host_update

--- Apartments/src/model/Guest.java
+++ Apartments/src/model/Guest.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Guest extends User {
+import java.util.List;
 
+public class Guest extends User {
+	
+	private List<Apartment> rentedApartments;
+	private List<Reservation> reservations;
+	
+	public Guest(List<Apartment> rentedApartments, List<Reservation> reservations) {
+		super();
+		this.rentedApartments = rentedApartments;
+		this.reservations = reservations;
+	}
+
+	public List<Apartment> getRentedApartments() {
+		return rentedApartments;
+	}
+
+	public void setRentedApartments(List<Apartment> rentedApartments) {
+		this.rentedApartments = rentedApartments;
+	}
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public void setReservations(List<Reservation> reservations) {
+		this.reservations = reservations;
+	}
+	
+	
 }
--- Apartments/src/model/Host.java
+++ Apartments/src/model/Host.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Host extends User {
+import java.util.List;
 
+public class Host extends User {
+	
+	private List<Apartment> apartmentsForRent;	
+	
+	public Host(List<Apartment> apartmentsForRent) {
+		super();
+		this.apartmentsForRent = apartmentsForRent;
+	}
+
+	public List<Apartment> getApartmentsForRent() {
+		return apartmentsForRent;
+	}
+
+	public void setApartmentsForRent(List<Apartment> apartmentsForRent) {
+		this.apartmentsForRent = apartmentsForRent;
+	}
+	
 }
