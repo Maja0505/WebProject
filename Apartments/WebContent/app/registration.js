08de6e3174a984cf170237466e046c931a7dd1aa
Maja Tomic <majatomic98@hotmail.com>
2020-09-01 04:18:20 +0200

    #registration_design

--- Apartments/WebContent/app/registration.js
+++ Apartments/WebContent/app/registration.js
@@ -9,115 +9,139 @@ Vue.component("registration", {
 	      errorGender:"",
 	      errorUsername:"",
 	      errorPassword:"",
-	      errorConfirmPassword:""
+	      errorConfirmPassword:"",
+	      loggedUser : null
 	    }
 },
 		template: ` 
 		<div>
 
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" method="post">
+					<span class="login100-form-title p-b-34">
+						Registration
+					</span>
+					
+					<div class="wrap-input100  validate-input m-b-20" data-validate="Type user name">
+						<input id="first-name" class="input100" type="text" name="username" placeholder="First name" v-model="user.firstName">
+						<span class="focus-input100"></span>
+						<p class="error-wrap-input100">{{errorFirstName}}</p>
+					</div>
+					<div class="wrap-input100  validate-input m-b-20" data-validate="Type password">
+						<input id="last-name" class="input100" type="text" name="username" placeholder="Last name" v-model="user.lastName">
+						<span class="focus-input100"></span>
+						<p class="error-wrap-input100">{{errorLastName}}</p>
+					</div>
+					<div class="wrap-input100  validate-input m-b-20" data-validate="Type password">
+				      <select class="browser-default custom-select input100" v-model="user.gender" >
+						    <option value="MALE" selected>Male</option>
+							<option value="FEMALE">Female</option>
+					  </select>
+						<span class="focus-input100"></span>
+						<p class="error-wrap-input100">{{errorGender}}</p>
+					</div>
+					<div class="wrap-input100  validate-input m-b-20" data-validate="Type user name">
+						<input id="username" class="input100" type="text" name="username" placeholder="Username" v-model="user.username">
+						<span class="focus-input100"></span>
+						<p class="error-wrap-input100">{{errorUsername}}</p>
+					</div>
+					<div class="wrap-input100  validate-input m-b-20" data-validate="Type password">
+						<input class="input100" type="password" name="pass" placeholder="Password" v-model="user.password">
+						<span class="focus-input100"></span>
+						<p class="error-wrap-input100">{{errorPassword}}</p>
+					</div>
+					<div class="wrap-input100  validate-input m-b-20" data-validate="Type password">
+						<input class="input100" type="password" name="pass" placeholder="Confirm password" v-model="confirmPassword">
+						<span class="focus-input100"></span>
+						<p class="error-wrap-input100">{{errorConfirmPassword}}</p>
+					</div>
+
+
+
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" v-on:click.prevent="checkForm(user, confirmPassword)">
+							Register
+						</button>
+					</div>
+
+					<div class="w-full text-center p-t-27 p-b-239">
+						<span class="txt1">
+							Already have an account?
+						</span>
+
+						<a href="#/login" class="txt2">
+							Sign in
+						</a>
+					</div>
+
 			
-<form>
-  <div class="container-registration">
-  	<label class="text-center-register">Registration</label>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
-	<div class="input-container-registration">
-    <label><b>First name</b></label><br>
-    <input name="FirstName" type="text" v-model="user.firstName" required>
-    <p  class="small-register-error">{{errorFirstName}}</p>
-    
+				</form>
 
-    <label><b>Last name</b></label><br>
-    <input name="LastName" type="text" v-model="user.lastName" required>
-    <p  class="small-register-error">{{errorLastName}}</p>
-    
-    <label><b>Gender</b></label><br>
-	<label class="radio-inline"><input type="radio" id="male" name="gender" value="MALE" v-model="user.gender">Male</label>
-	<label class="radio-inline"><input type="radio" id="female" name="gender" value="FEMALE" v-model="user.gender">Female</label></br>
-	<p  class="small-register-error">{{errorGender}}</p>
-
-    
-    
-    
-    <label><b>Username</b></label><br>
-    <input name="Username" type="text" v-model="user.username" required>
-	<p  class="small-register-error">{{errorUsername}}</p>
-	
-
-	
-
-    <label><b>Password</b></label><br>
-    <input name="Password" type="password" v-model="user.password" required>
-	<p  class="small-register-error">{{errorPassword}}</p>
-	
-    <label><b>Repeat Password</b></label><br>
-    <input name="ConfirmPassword" type="password" v-model="confirmPassword" required>
-     <p  class="small-register-error">{{errorConfirmPassword}}</p>
-     
-    </div>
-    <hr>
-    	
-    <button type="submit" class="registerbtn" v-on:click="checkForm(user, confirmPassword)">Register</button>
-  </div>
-  
-  <div class="container-registration signin">
-    <p>Already have an account? <a href="#/login">Sign in</a>.</p>
-  </div>
-</form>
-			
-			
+				<div class="login100-more" style="background-image: url('images/apartment1.png');"></div>
+			</div>
 		</div>
+	</div>
+
+
+</div>
 		`
 		,methods : {
 			checkForm : function (user, confirmPassword) {
-				this.errorFirstName = "";
-			    this.errorLastName = "";
-		        this.errorGender = "";
-		        this.errorUsername = "";
-		        this.errorPassword = "";
-		        this.errorConfirmPassword = "";
-				this.user = user;
-				this.confirmPassword = confirmPassword;
-				if(!user.firstName){
-					this.errorFirstName = "First name can't be empty";
-				}
-				if(!user.lastName){
-				    this.errorLastName = "Last name can't be empty";
-				}
-				if(!user.gender){
-			        this.errorGender = "Gender can't be empty";
-
-				}
-				if(!user.username){
-			        this.errorUsername = "Username can't be empty";
-
-				}
-				if(!user.password){
-					this.errorPassword = "Password can't be empty";
-
-				}
-				if(!confirmPassword){
-			        this.errorConfirmPassword = "Confirm password can't be empty";
-
-				}
 				
-				if(!this.errorFirstName && !this.errorLastName && !this.errorGender && !this.errorUsername && !this.errorPassword && !this.errorConfirmPassword){
-					if(user.password.length < 8){
-						this.errorPassword = "Your password must contain at least 8 characters ";
-						return;
+				 this.$nextTick(function(){	
+					 	this.errorFirstName = "";
+					    this.errorLastName = "";
+				        this.errorGender = "";
+				        this.errorUsername = "";
+				        this.errorPassword = "";
+				        this.errorConfirmPassword = "";
+						this.user = user;
+						this.confirmPassword = confirmPassword;
+						if(!user.firstName){
+							this.errorFirstName = "First name can't be empty";
+						}
+						if(!user.lastName){
+						    this.errorLastName = "Last name can't be empty";
+						}
+						if(!user.gender){
+					        this.errorGender = "Gender can't be empty";
+
+						}
+						if(!user.username){
+					        this.errorUsername = "Username can't be empty";
+
+						}
+						if(!user.password){
+							this.errorPassword = "Password can't be empty";
+
+						}
+						if(!confirmPassword){
+					        this.errorConfirmPassword = "Confirm password can't be empty";
+
+						}
+						if(!this.errorFirstName && !this.errorLastName && !this.errorGender && !this.errorUsername && !this.errorPassword && !this.errorConfirmPassword){
+							if(user.password.length < 8){
+								this.errorPassword = "Your password must contain at least 8 characters ";
+								return;
+							}
+							if(user.password != confirmPassword){
+							   this.errorConfirmPassword = "Password and Confirm password doesn't match";
+							   return;
+							}
+						}
+					 
+					 
+					 if(user.firstName && user.lastName && user.gender && user.username && user.password && confirmPassword){
+						axios
+				          .get('rest/users/all')
+				          .then(response => (this.users = response.data,
+				        		  this.userExists()))
 					}
-					if(user.password != confirmPassword){
-					   this.errorConfirmPassword = "Password and Confirm password doesn't match";
-					   return;
-					}
-				}
-				
-				if(user.lastName && user.lastName && user.gender && user.username && user.password && confirmPassword){
-					axios
-			          .get('rest/users/all')
-			          .then(response => (this.users = response.data,
-			        		  this.userExists()))
-				}
+			})
+					
 	            
 				},
 			login : function () {
@@ -129,7 +153,10 @@ Vue.component("registration", {
 				            'Content-Type': 'application/json',
 				        }
 		        		  })
-		          .then(response => (this.$router.push('/')  ))
+		          .then(response => ( 
+		        		  axios
+					      .get('rest/users/currentUser')
+					      .then(response => (response.data ? this.loggedUser = response.data : this.loggedUser = null,this.$root.$emit('changeCurentUser',this.loggedUser))), this.$router.push('/')))
 				},
 				
 			userExists : function(){
